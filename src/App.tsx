e76d4abfaeb102644f88f2cead752038097e0b4b
Manish kumar <manojgeetparmar@gmail.com>
2024-01-09 10:06:49 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+
+// here React.FC stands for react functional component.
+const App: React.FC = () => {
+  return <div className="App">Hello world</div>;
+};
+
+export default App;
